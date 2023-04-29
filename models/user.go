457ee81a89b9182da024b0958dd2af0f0179f6d3
MirInjamamul <injamul120221@gmail.com>
2023-04-29 13:21:49 +0600

    file merged

--- models/user.go
+++ models/user.go
@@ -13,3 +13,12 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+type Complain struct {
+	ID           uint      `json:"id" gorm:"primary_key"`
+	Thana        string    `json:"thana"`
+	ComplainType string    `json:"complain_type"`
+	Comment      string    `json:"comment"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
